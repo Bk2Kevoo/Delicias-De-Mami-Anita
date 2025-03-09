354c2c1a17b67657bd6a4844d8dd8f85bd427555
+ <+>
2025-03-08 19:07:14 -0500

    Changed input

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets', // Optional: define the folder for assets (images, CSS, etc.)
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: '/public/index.html',  
+    },
+  },
+  server: {
+    historyApiFallback: true, 
   },
 });
